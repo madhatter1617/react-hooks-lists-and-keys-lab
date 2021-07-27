a4f6f7290736513faed8f7d8f8a581fa9ae4f7ac
Rosa <rosarenee@att.net>
2021-07-26 23:06:55 -0500

    Done biss!

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+      {projects.map((proj) => {
+          return <div key={proj.id} >
+            <ProjectItem             
+            name={proj.name} 
+            about={proj.about} 
+            technologies={proj.technologies}/>;
+            </div>
+        })} 
+      </div>
     </div>
   );
 }
